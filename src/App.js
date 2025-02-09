e34a3fe12bb9332c506b3b4693deaabfbbc5679d
quangvietung <quangviet.ung@georgebrown.ca>
2025-02-09 09:13:48 -0500

    including application source

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Student ID: 101535255</h1>
+      <h1>Student name: Quang Viet Ung</h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developerd By: Quang Viet Ung
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
